fd4a8832c88d16dc3a5316578cb17df63712ba5b
catoja1107 <jcato195@gmail.com>
2017-03-03 12:52:17 -0500

    Add files via upload
    Initial Files

---
+++ TSA_State_SOL/TSA_State/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TSA_State
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = Directory.GetCurrentDirectory();
+            webBrowser1.Navigate(path+"\\html\\index.html");
+
+        }
+    }
+}
---
+++ TSA_State_SOL/TSA_State/bin/Debug/html/js/main.js
@@ -0,0 +1,3 @@
+function redirect(){
+	document.getE
+}
